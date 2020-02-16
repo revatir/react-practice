6a748d67087225603513d37ee69469ce1d52bde0
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-02-16 11:02:12 -0500

    completed practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+
+//CSS
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = ({
+      users: [],
+      modal: false,
+      clickedId: "",
+    })
+  }
+
+  componentDidMount = async () => {
+    let users = await axios('https://randomuser.me/api/?results=15&inc=name,picture,%20login')
+    this.setState({
+      users: users.data.results
+    })
+  }
+
+  openModal = (e, uuid) => {
+    // e.stopPropagation()
+    if (this.state.modal === false) {
+      this.setState({
+        modal: true,
+        clickedId: uuid
+      })
+    }
+    // else {
+    //   this.setState({
+    //     modal: false,
+    //     clickedId: ""
+    //   })
+    // }
+  }
+
+  closeModal = (e) => {
+    // e.stopPropagation()
+    if (this.state.modal === true) {
+      this.setState({
+        modal: false,
+        clickedId: ""
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <ul>
+          {this.state.users.map(user => (
+            <li onClick={(e) => this.openModal(e, user.login.uuid)} className="user">
+              <p>{user.name.first} {user.name.last}</p>
+              <img src={user.picture.thumbnail} alt="user" />
+              {this.state.modal === true && user.login.uuid === this.state.clickedId &&
+                <div>
+                  <div className="modal-background" onClick={(e) => this.closeModal(e)}></div>
+                  <div className="modal">
+                    <p onClick={(e) => this.closeModal(e)} >X</p>
+                    <p>{user.name.first} {user.name.last}</p>
+                    <img src={user.picture.thumbnail} alt="user" />
+                  </div>
+                </div>
+              }
+            </li>
+          ))}
+        </ul>
+      </div >
+    );
+  }
 }
 
 export default App;
